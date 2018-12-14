2bbf259c85966318bb329335f0bb9b980babe912
MaxKlaxxMiner <max@tetrisk.de>
2018-12-14 20:16:20 +0100

    +i2s

---
+++ usb_i2s/usb_i2s.ino
@@ -0,0 +1,24 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputUSB            usb1;           //xy=465,429
+AudioOutputI2S           i2s1;           //xy=826,421
+AudioConnection          patchCord1(usb1, 0, i2s1, 0);
+AudioConnection          patchCord2(usb1, 1, i2s1, 1);
+// GUItool: end automatically generated code
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  AudioMemory(12);
+}
+
+void loop()
+{
+  
+}
+
